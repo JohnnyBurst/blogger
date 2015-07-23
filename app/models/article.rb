443822209da5b05b152c77be2342b04c5ac8f46b
Johnny <johnjritenour@gmail.com>
2015-07-23 21:10:16 +0000

    working on tags

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,15 @@
 class Article < ActiveRecord::Base
     has_many :comments
+    has_many :taggings
+    has_many :tags, through: :taggings
+    
+    def tag_list
+        tags.join(", ")
+    end
+    
+    def tag_list=(tags_string)
+    
+    end
+    
+    
 end
